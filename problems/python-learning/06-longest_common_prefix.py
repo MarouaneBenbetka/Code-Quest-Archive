dd823f7eacb25692cd25a1a59a6c31ad9a2a0cf0
Marouane <MarouaneBenbetka@gmail.com>
2024-04-04 15:42:54 +0100

    problem: longest common prefix

---
+++ problems/python-learning/06-longest_common_prefix.py
@@ -0,0 +1,13 @@
+class Solution:
+    def longestCommonPrefix(self, strs):
+
+        n = min(len(s) for s in strs)
+        prefix = ""
+        for i in range(n):
+            l = strs[0][i]
+            if all(s[i] == l for s in strs):
+                prefix += l
+            else:
+                return prefix
+
+        return prefix
