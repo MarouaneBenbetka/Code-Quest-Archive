7ff161c290e7341e4b846ef43ce6442c3c2ad499
Marouane <MarouaneBenbetka@gmail.com>
2024-04-05 16:22:35 +0100

    problem: non decreasing array

--- problems/python-learning/33-non_decreasing_array.py
+++ problems/python-learning/33-non_decreasing_array.py
@@ -1,25 +1,22 @@
 
 
 # https://leetcode.com/problems/non-decreasing-array/
-
 class Solution:
-    def checkPossibility(self, nums):
+    def checkPossibility(self, nums: List[int]) -> bool:
 
         can_modify = 1
-        i = 0
-        while i < len(nums)-1:
+        for i in range(len(nums)-1):
             if nums[i] <= nums[i+1]:
-                i += 1
                 continue
             elif can_modify:
                 can_modify -= 1
-                if i > 0:
+                if i == 0:
+                    nums[i] = float('-inf')
+                else:
                     if nums[i-1] <= nums[i+1]:
                         nums[i] = nums[i-1]
                     else:
                         nums[i+1] = nums[i]
-                else:
-                    nums[i] = float('-inf')
             else:
                 return False
         return True
