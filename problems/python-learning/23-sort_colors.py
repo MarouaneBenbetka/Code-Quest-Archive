ee578f8abb70193071c4f0e4029ff09368b710f9
Marouane <MarouaneBenbetka@gmail.com>
2024-04-05 01:14:55 +0100

    problem: sort colors

---
+++ problems/python-learning/23-sort_colors.py
@@ -0,0 +1,14 @@
+class Solution:
+    def sortColors(self, nums):
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        d = [0, 0, 0]
+        for i in nums:
+            d[i] += 1
+
+        cpt = 0
+        for color, color_count in enumerate(d):
+            for j in range(color_count):
+                nums[cpt] = color
+                cpt += 1
