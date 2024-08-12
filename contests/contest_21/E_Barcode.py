19282c35db3a425ea90753df62d558f8ffbeb678
Marouane <MarouaneBenbetka@gmail.com>
2024-08-12 23:53:05 +0100

    update solution

--- contests/contest_21/E_Barcode.py
+++ contests/contest_21/E_Barcode.py
@@ -2,32 +2,6 @@ import sys
 import threading
 
 
-def solve(curr_col, prev_color, x, y, dp, pfx_white, pfx_black, m):
-    if curr_col == m:
-        return 0
-
-    if (curr_col, prev_color) in dp:
-        return dp[(curr_col, prev_color)]
-
-    minn = float('inf')
-
-    for end in range(curr_col + x, min(curr_col + y + 1, m + 1)):
-        white_count, black_count = pfx_white[end] - \
-            pfx_white[curr_col], pfx_black[end] - pfx_black[curr_col]
-
-        if prev_color == 'B':
-            cost = white_count + \
-                solve(end, 'W', x, y, dp, pfx_white, pfx_black, m)
-        else:
-            cost = black_count + \
-                solve(end, 'B', x, y, dp, pfx_white, pfx_black, m)
-
-        minn = min(minn, cost)
-
-    dp[(curr_col, prev_color)] = minn
-    return minn
-
-
 def main():
 
     n, m, x, y = map(int, input().split())
@@ -37,15 +11,37 @@ def main():
     pfx_white = [0] * (m + 1)
     pfx_black = [0] * (m + 1)
 
-    for j in range(1, m + 1):
-        white_px = sum(bar_code[i][j-1] == '.' for i in range(n))
-        pfx_white[j] = pfx_white[j - 1] + white_px
-        pfx_black[j] = pfx_black[j - 1] + n - white_px
+    for j in range(m):
+        white_px = sum(bar_code[i][j] == '.' for i in range(n))
+        pfx_white[j+1] = pfx_white[j] + white_px
+        pfx_black[j+1] = pfx_black[j] + n - white_px
 
-    dp = {}
+    memo = {}
 
-    print(min(solve(0, 'B', x, y, dp,  pfx_white, pfx_black, m),
-          solve(0, 'W',  x, y, dp, pfx_white, pfx_black, m)))
+    def solve(curr_col, curr_color):
+        if curr_col == m:
+            return 0
+
+        if (curr_col, curr_color) in memo:
+            return memo[(curr_col, curr_color)]
+
+        minn = float('inf')
+
+        for end in range(curr_col + x, min(curr_col + y + 1, m + 1)):
+
+            if curr_color == 'B':
+                white_count = pfx_white[end] - pfx_white[curr_col]
+                cost = white_count + solve(end, 'W')
+            else:
+                black_count = pfx_black[end] - pfx_black[curr_col]
+                cost = black_count + solve(end, 'B')
+
+            minn = min(minn, cost)
+
+        memo[(curr_col, curr_color)] = minn
+        return minn
+
+    print(min(solve(0, 'B'), solve(0, 'W')))
 
 
 sys.setrecursionlimit(1 << 30)
